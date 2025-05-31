97de96a10169de3d30b0707e3bc494762d83f440
tomvanacker85 <49290930+tomvanacker85@users.noreply.github.com>
2025-05-31 11:41:16 +0200

    change install.sh and restart.sh

--- dbus-homewizard-energy-p1/install.sh
+++ dbus-homewizard-energy-p1/install.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  )
-DAEMON_NAME=${SCRIPT_DIR##*/}
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SERVICE_NAME=$(basename $SCRIPT_DIR)
+
+echo
+echo "Installing $SERVICE_NAME..."
 
 # set permissions for script files
-chmod a+x $SCRIPT_DIR/restart.sh
-chmod 744 $SCRIPT_DIR/restart.sh
-
-chmod a+x $SCRIPT_DIR/uninstall.sh
-chmod 744 $SCRIPT_DIR/uninstall.sh
-
-chmod a+x $SCRIPT_DIR/service/run
+echo "Setting permissions..."
+chmod 755 $SCRIPT_DIR/*.py
+chmod 755 $SCRIPT_DIR/*.sh
 chmod 755 $SCRIPT_DIR/service/run
-
-
+chmod 755 $SCRIPT_DIR/service/log/run
 
 # create sym-link to run script in deamon
-ln -s $SCRIPT_DIR/service /service/$DAEMON_NAME
-
-
+if [ ! -L /service/$SERVICE_NAME ]; then
+    echo "Creating service..."
+    ln -s $SCRIPT_DIR/service /service/$SERVICE_NAME
+else
+    echo "Service already exists."
+fi
 
 # add install-script to rc.local to be ready for firmware update
 filename=/data/rc.local
@@ -29,4 +30,7 @@ then
     echo >> $filename
 fi
 
-grep -qxF "$SCRIPT_DIR/install.sh" $filename || echo "$SCRIPT_DIR/install.sh" >> $filename
+# if not alreay added, then add to rc.local
+grep -qxF "bash $SCRIPT_DIR/install.sh" $filename || echo "bash $SCRIPT_DIR/install.sh" >> $filename
+
+echo
--- dbus-homewizard-energy-p1/restart.sh
+++ dbus-homewizard-energy-p1/restart.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-DAEMON_NAME=${SCRIPT_DIR##*/}
+SERVICE_NAME=$(basename $SCRIPT_DIR)
 
-kill $(pgrep -f "python $SCRIPT_DIR/$DAEMON_NAME")
+echo
+echo "Restarting $SERVICE_NAME..."
+
+pid=$(pgrep -f "python $SCRIPT_DIR/$SERVICE_NAME.py")
+if [ -n "$pid" ]; then
+    svc -t /service/$SERVICE_NAME
+    pkill -f "python $SCRIPT_DIR/$SERVICE_NAME.py" > /dev/null 2>&1
+    echo "done."
+else
+    echo "driver is not running!"
+fi
+
+echo
